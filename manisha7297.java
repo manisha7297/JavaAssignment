c308ac076c0f326ee04f02a8ae2b29043d703b9a
manisha7297 <37671240+manisha7297@users.noreply.github.com>
2018-08-19 00:45:46 +0530

    adding manisha7297.java

---
+++ manisha7297.java
@@ -0,0 +1,19 @@
+class AndroidWorkDay{
+public static void main(String [] args)throws Exception
+{
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+int i, n = Integer.parseInt(br.readLine());
+String[] m = new string[n];
+for(i=0;i<n;i++)
+{
+m[i]=br.readLine();
+}
+for(i=0;i<n;i++)
+{
+for(int j=-1;(j=str.indexOf(m[i],j+1)) != -1;)
+{
+System.out.println(j="");
+}
+}
+}
+}
